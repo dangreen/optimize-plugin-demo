7feaa99651624d198bfee123db57fe1dc6f25572
dangreen <danon0404@gmail.com>
2022-01-10 20:45:48 +0700

    init

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import pLimit from 'p-limit';
+import axios from 'axios';
+import * as svelte from 'svelte';
+
+console.log(pLimit, axios, svelte);
---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const OptimizePlugin = require('optimize-plugin');
+
+const { VARIANT } = process.env;
+const VariantSettings = {
+  'optimize-plugin': () => ({
+    plugins: [
+      new OptimizePlugin({
+        minify: true,
+        downlevel: true,
+        concurrency: false
+      })
+    ]
+  }),
+  'babel-modern': () => ({
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  '@babel/preset-env',
+                  {
+                    targets: 'defaults and supports es6-module'
+                  }
+                ]
+              ]
+            }
+          }
+        }
+      ]
+    }
+  }),
+  'babel-legacy': () => ({
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  '@babel/preset-env',
+                  {
+                    targets: 'defaults',
+                    corejs: 3,
+                    useBuiltIns: 'usage'
+                  }
+                ]
+              ]
+            }
+          }
+        }
+      ]
+    }
+  })
+};
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    filename: `${VARIANT}.js`
+  },
+  ...(VariantSettings[VARIANT] ? VariantSettings[VARIANT]() : {})
+};
